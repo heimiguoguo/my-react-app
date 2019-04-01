090e9bc1c386f1964998fc7018b70dcd0ef052a4
heimiguoguo <mang.huang@hexagon.com>
2019-04-01 17:25:49 +0800

    imitate ElmentUI to implement react components

--- src/DragulaDemo.js
+++ src/DragulaDemo.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import * as ReactDOM from 'react-dom';
 import 'dragula/dist/dragula.css';
 import Dragula from 'react-dragula';
 import './DragulaDemo.css';
