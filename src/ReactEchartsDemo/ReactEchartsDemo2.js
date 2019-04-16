207733a4a899e200d2797bd17d4385ac54c0f373
heimiguoguo <mang.huang@hexagon.com>
2019-04-16 09:56:16 +0800

    finish todo demo using redux

--- src/ReactEchartsDemo/ReactEchartsDemo2.js
+++ src/ReactEchartsDemo/ReactEchartsDemo2.js
@@ -7,6 +7,15 @@ class ReactEchartsDemo extends Component{
         return {
             title: { text: 'ECharts 入门示例' },
             tooltip: {},
+            toolbox:{
+                itemSize:16,
+                feature:{
+                    saveAsImage:{
+                        title:'save',
+                        icon: 'image:// /peiqi.jpg'
+                    }
+                }
+            },
             xAxis: {
                 data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
             },
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import App from './reactstrap/customer/index';
+// import App from './reactstrap/customer/index';
+// import App from './react-router/components/example';
+// import App from './ReactEchartsDemo/ReactEchartsDemo2';
+
+import todoApp from './redux/reducers'
+import App from './redux/components/App'
 import * as serviceWorker from './serviceWorker';
 
+let store = createStore(todoApp)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
