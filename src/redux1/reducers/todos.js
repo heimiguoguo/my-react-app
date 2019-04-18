0299a28367ee28a6afd709e21c91b1bc069dce8a
heimiguoguo <mang.huang@hexagon.com>
2019-04-18 17:35:09 +0800

    do redux demo again

---
+++ src/redux1/reducers/todos.js
@@ -0,0 +1,46 @@
+// export function todos(state=[], action){
+//     switch(action.type){
+//         case 'ADD_TODO':
+//             return [...state, {
+//                 id: action.id,
+//                 text: action.text,
+//                 completed: false
+//             }]
+//         case 'TOGGLE_TODO':
+//             return state.map((todo, index) => {
+//                 if(index === action.index){
+//                     return Object.assign({}, todo, {
+//                         completed: !todo.completed
+//                     })
+//                 }
+//                 return todo
+//             })
+//         default:
+//             return state
+//     }
+// }
+
+const todos = (state = [], action) => {
+    switch(action.type){
+        case 'ADD_TODO':
+            return [...state, {
+                id: action.id,
+                text: action.text,
+                completed: false
+            }]
+        case 'TOGGLE_TODO':
+            return state.map(todo => {
+                if(todo.id === action.id){
+                    return Object.assign({}, todo, {
+                        completed: !todo.completed
+                    })
+                }
+                return todo
+            })
+        default:
+            return state
+    }
+}
+
+
+export default todos
