8bde7707c36c72291a421ee83b7cb93e516ca4d5
Tom <mang.huang@hexagon.com>
2020-06-11 16:41:20 +0800

    Import redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd/dist/antd.css';
 // import App from './App';
-import App from './components/app'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './redux/reducers'
+import App from './redux/components/App'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(< App />, document.getElementById('root'));
+let store = createStore(todoApp)
+
+ReactDOM.render(
+    <Provider store={store}>
+        < App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
